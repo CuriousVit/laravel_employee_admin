adce3911e59830306f2fe45401c9e20d036e367c
CuriousVit <techitif@gmail.com>
2017-09-22 21:30:12 -0700

    Employee table seeder

---
+++ database/seeds/EmployeesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Employee;
+
+class EmployeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        Employee::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Employee::create([
+                'name' => $faker->name,
+                'date_of_birth' => $faker->date('Y-m-d'),
+                'address' => $faker->address,
+            ]);
+        }
+    }
+}
