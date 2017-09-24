6d03741921550b9bbd6515c5e61fb17be685ea4c
CuriousVit <techitif@gmail.com>
2017-09-23 23:37:22 -0700

    Create layout.blade.php to use for all templates

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!-- layout.blade.php -->
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="app" class="container">
+
+    @yield('content')
+
+</div> <!-- /container -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
+<script src="{{asset('js/app.js')}}"></script>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
+
+</body>
+</html>
